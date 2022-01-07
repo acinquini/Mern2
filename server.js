bb97bd0834c8a79af3f8f7f232a73eecdc1785bc
Alessandro <acinquini@gmail.com>
2022-01-07 16:05:29 +0100

    First deployment in heroku

--- server.js
+++ server.js
@@ -14,6 +14,17 @@ app.use(express.json({ extended: false }));
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 
+const { PORT, HOST, NODE_ENV } = process.env;
+
+// production
+if (NODE_ENV === 'production') {
+    console.log("NODE_ENV = production")
+    app.use(express.static('client/build'));
+  
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+  }
+
 // Note: app.listen creates the server
-const { PORT, HOST } = process.env;
 app.listen(PORT, HOST, () => console.log(`Server started on ${HOST} at port ${PORT} `));
